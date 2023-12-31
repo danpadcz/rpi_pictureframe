3d25c3a2a4be29ef2b01a015783a6eaf3d06dc43
Daniel Padrta <danielpadrta@gmail.com>
2023-12-31 14:40:13 +0100

    Add library

--- photoframe_script.py
+++ photoframe_script.py
@@ -8,7 +8,7 @@ import os
 #     sys.path.append(libdir)
 
 import logging
-from waveshare_epd import epd7in5_V2
+import epd7in5_V2
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
